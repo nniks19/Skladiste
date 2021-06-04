0f13f6539f68bbe1ed082acf0225bd1c31a5e968
nniks19 <77620949+nniks19@users.noreply.github.com>
2021-06-04 17:08:20 +0200

    Tables are now printing through angularJS
    All tables are getting data from angularJS. I have to work on modals and buttons to have functionality....

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@
 
 var oSkladisteModul = angular.module('skladiste-app', ['datatables']);
 
+oSkladisteModul.controller('listaArtikala', ['$scope', '$http', function ($scope, $http){
+	$http({
+		method:'get',
+		url: 'action/action.php?action_id=get_artikli'
+	}).then(function successCallback(response){
+		$scope.Artikli = response.data;
+	});
+}]);
+
 //direktive modula skladiste-app
 
 oSkladisteModul.directive("headernav",
@@ -27,21 +36,58 @@ oSkladisteModul.directive("login",
 	}
 )
 
-// controller liste artikala iz modula skladiste-app
-
-oSkladisteModul.controller('listaArtikala', ['$scope', '$http', function ($scope, $http){
-	$http({
-		method:'get',
-		url: 'action/action.php?action_id=get_artikli'
-	}).then(function successCallback(response){
-		$scope.Artikli = response.data;
-	});
-}]);
 
 //modul skladiste-panel
 
 var oSkladistePanelModul = angular.module('skladiste-panel', ['datatables']);
 
+oSkladistePanelModul.controller('ctablicaArtikli', ['$scope', '$http', function ($scope, $http){
+		$scope.ucitajArtikle = function () {
+			$http({
+			method:'get',
+			url: '../action/action.php?action_id=get_artikli_crud'
+		}).then(function successCallback(response){
+			$scope.Artikli = response.data;
+		});
+	}
+	$scope.ucitajArtikle();
+}]);
+
+oSkladistePanelModul.controller('ctablicaPrimke', ['$scope', '$http', function ($scope, $http){
+	$scope.ucitajPrimke = function(){
+		$http({
+			method:'get',
+			url: '../action/action.php?action_id=get_primke_crud'
+		}).then(function successCallback(response){
+			$scope.Primke = response.data;
+		});
+	}
+	$scope.ucitajPrimke();
+}]);
+
+oSkladistePanelModul.controller('ctablicaIzdatnice', ['$scope', '$http', function ($scope, $http){
+	$scope.ucitajIzdatnice = function(){
+		$http({
+			method:'get',
+			url: '../action/action.php?action_id=get_izdatnice_crud'
+		}).then(function successCallback(response){
+			$scope.Izdatnice = response.data;
+		});
+	}
+	$scope.ucitajIzdatnice();
+}]);
+
+oSkladistePanelModul.controller('ctablicaKategorije', ['$scope', '$http', function ($scope, $http){
+	$scope.ucitajKategorije = function(){
+		$http({
+			method:'get',
+			url: '../action/action.php?action_id=get_kategorije_crud'
+		}).then(function successCallback(response){
+			$scope.Kategorije = response.data;
+		});
+	}
+	$scope.ucitajKategorije();
+}]);
 
 oSkladistePanelModul.directive("headerpanelnav",
 	function(){
@@ -50,3 +96,4 @@ oSkladistePanelModul.directive("headerpanelnav",
 			templateUrl: "../templates/navbarpanel.html"
 		};
 });
+
