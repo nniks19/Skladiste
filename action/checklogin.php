de4018f6b5401ffe7b14a61814b6948212182e5a
nniks19 <77620949+nniks19@users.noreply.github.com>
2021-06-07 21:44:14 +0200

    reupload - action directory
    New way of handfling things.
    Will update secure way in future.

--- action/checklogin.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../connection.php';
-session_start();
-// Ako korisnik nije prijavljen prebacujem ga na stranicu za Prijavu
-if (!isset($_SESSION['id'])) {
-	header('Location: ../prijava.php');
-	exit;
-}
-?>
